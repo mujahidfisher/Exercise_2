f8701d2ea8ef20fad38711dc8c3ab0f8ad07390b
mujahidfisher <fishermujahid@gmail.com>
2023-07-31 15:44:14 +0200

    finished

---
+++ index.js
@@ -0,0 +1,7 @@
+let Person = require("./person");
+
+let person1 = new Person("Mujahid", "Fisher", "fishermujahid@gmail.com");
+let person2 = new Person("Joel", "Mukunya", "joel@lifechoice.com");
+let person3 = new Person("Amy", "Adams", "Amyboo@gmail.com");
+
+console.log(person1, person2, person3);
---
+++ person.js
@@ -0,0 +1,9 @@
+function Person(firstName, lastName, email) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.email = email
+}
+
+module.exports = Person
+
+
